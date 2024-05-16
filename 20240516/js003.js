4b50728d18ddd88f7c87d6cd75f2f2e2420a81dd
Epi0702 <rnqhsrhkd@gmail.com>
2024-05-16 17:25:36 +0900

    20240516 JavaScript class

---
+++ 20240516/js003.js
@@ -0,0 +1,8 @@
+let num = 0;
+while (num < 11) {
+    num++;
+    document.write(num, '<br>');
+    if(num > 5){
+        break;
+    }
+}
---
+++ 20240516/js004.js
@@ -0,0 +1,9 @@
+let i = 0;
+while (i < 100) {
+    i++;
+    if (i === 14) {
+        console.log(i + '살 부터 중학생이 됩니다.');
+        break;
+    }
+}
+console.log('중학교 입학을 축하합니다');
